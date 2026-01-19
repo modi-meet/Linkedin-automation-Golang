1545de75a5bab80e44dcaab4c0432331ab4f6a8c
Modi Meet <meetmodi8080@gmail.com>
2026-01-19 22:36:34 +0530

    fix: add godotenv to load .env file on startup

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"github.com/meetm/linkedin-automation-go/api"
 	"github.com/meetm/linkedin-automation-go/pkg/logger"
 )
 
 func main() {
-	// Initialize logger
+	godotenv.Load()
+
 	log := logger.New()
 
-	// Initialize and start API server
 	server := api.NewServer(log)
 	server.Start()
 }
